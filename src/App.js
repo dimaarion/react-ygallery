7c63f01d6bfdb37aa73ef499857fdc28a618eefb
dimaarion <dimaarion@gmail.com>
2020-10-14 00:47:26 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./style.css";
 import "./bootstrap.css";
 export default function App() {
+ 
+    console.log(document.getElementById('images') + 'wefwefwef')
   return (
-    <div className = "container-fluid">
-     
+    <div className="container-fluid">
+      <img src = "" />
     </div>
   );
 }
